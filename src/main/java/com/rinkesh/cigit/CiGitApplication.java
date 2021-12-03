2fb0389c2d97e083e7a34f07a9dd8e5fd3e805b6
Rinkesh Mehar <rinkesh.mehar@cropdata.in>
2021-12-03 17:41:27 +0530

    added initial commit

---
+++ src/main/java/com/rinkesh/cigit/CiGitApplication.java
@@ -0,0 +1,13 @@
+package com.rinkesh.cigit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiGitApplication.class, args);
+    }
+
+}
